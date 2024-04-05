56172d24aa744c7e722941261272fa622cd2268f
u3612777 <158137742+u3612777@users.noreply.github.com>
2024-04-05 23:33:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<sstream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<ifstream>
 
+int main(){
+  cout<<"Hello world!"<<endl;
+  return 0;
+}
